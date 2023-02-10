2247bd01a8ef4150079b55815281927333f56d79
jssmith79 <i.am.hef@gmail.com>
2023-02-09 19:04:17 -0800

    code along

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,40 @@
 
 // Your Code Here
+async function main() {
+
+    let response = await fetch('http://localhost:3001/listBooks')
+    let books = await response.json()
+
+    books.forEach(renderBook)
+}
+
+function renderBook(book) {
+ 
+    let root = document.getElementById("root")
+    let listItem = document.createElement("li")
+    let qtyInput = document.createElement("input")
+    listItem.textContent(`${book.title}`)
+    qtyInput.setAttribute('value' = `${book.quantity}`)
+    saveBtn.textContent = "Save"
+    
+    saveBtn.addEventListener('click', () => {
+        fetch('http://localhost:3001/updateBook'),{
+        method: "PATCH"
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            id: book.id,
+            quantity: qtyInput.value
+        })
+    })
+
+})
+    listItem.append(qtyInput, saveBtn)
+    
+    root.append(listItem)
+
+
+}
+
+main()
